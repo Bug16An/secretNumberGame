fbe822ff3886ff1a5048b9e1ddedc3f8a9e80190
Bug16An <junior.h.ferrante@gmail.com>
2024-02-11 01:04:34 -0300

    numero secreto reinicializado

---
+++ js-curso-2-aula_-/app.js
@@ -0,0 +1,57 @@
+// let title = document.querySelector('h1');
+// title.innerHTML = 'Secret number game';
+
+// let paragraph = document.querySelector('p');
+// paragraph.innerHTML = 'Choose a number betwen 1 and 100';
+let secretNumber = generateRandomNumber();
+let atempts = 1;
+
+function displayTextOnScreen (tag, text){
+    let camp = document.querySelector(tag);
+    camp.innerHTML = text;
+}
+
+function displayInitialMessage(){
+    displayTextOnScreen('h1', 'Secret number game');
+    displayTextOnScreen('p', 'Choose a number betwen 1 and 100');
+}
+displayInitialMessage();
+
+function checkGuess () {
+    let guess = document.querySelector('input').value;
+
+    if (guess == secretNumber){
+        displayTextOnScreen('h1', 'Right');
+        let atemptsWord = atempts > 1 ? 'Atempts' : 'Atempt';
+        let messageAtempts = `you discovered the secret number with ${atempts} ${atemptsWord}!`;
+        displayTextOnScreen('p', messageAtempts);
+        document.getElementById ('reiniciar').removeAttribute('disabled');
+    }else {
+        if (guess > secretNumber){
+            displayTextOnScreen('p', 'The secret number is smaller');
+        }else{
+            displayTextOnScreen('p', 'The secret number is bigger');
+        }
+        atempts ++;
+        cleanCamp();
+    }
+
+}
+
+function generateRandomNumber() {
+    return parseInt(Math.random() * 100 + 1);
+}
+
+function cleanCamp () {
+    guess = document.querySelector('input');
+    guess.value = '';
+
+}
+
+function restartGame(){
+    secretNumber = generateRandomNumber();
+    cleanCamp();
+    atempts = 1;
+    displayInitialMessage();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
